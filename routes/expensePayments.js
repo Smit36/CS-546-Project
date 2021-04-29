4925b4e9ac6171ad4dedb3db9d524d92de584b23
Smit Gor <smitgor1620@gmail.com>
2021-04-28 21:33:15 -0400

    error handling in routes

--- routes/expensePayments.js
+++ routes/expensePayments.js
@@ -4,48 +4,48 @@ const router = Router();
 const paymentData = require('../data/expensePayments');
 
 //Add Expense Payment
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const expensePayment = req.body;
     const newExpense = await paymentData.addExpensePayment(expensePayment);
     res.status(200).json(newExpense);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Get Expense Payment
-router.get('/:paymentId', async (req, res) => {
+router.get('/:paymentId', async (req, res, next) => {
   try {
     const { paymentId } = req.params;
     const payment = await paymentData.getExpensePayment(paymentId);
     res.status(200).json(payment);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Update Expense Payment
-router.put('/:paymentId', async (req, res) => {
+router.put('/:paymentId', async (req, res, next) => {
   try {
     const { paymentId } = req.params;
     let expensePayment = req.body;
 
     updatedPayment = await paymentData.updateExpensePayment(paymentId, expensePayment);
     res.status(200).json(updatedPayment);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Delete Expense Payment
-router.delete('/:paymentId', async (req, res) => {
+router.delete('/:paymentId', async (req, res, next) => {
   try {
     const { paymentId } = req.params;
     const expensePayment = await paymentData.deleteExpensePayment(paymentId);
     res.status(200).json(expensePayment);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
--- routes/expenses.js
+++ routes/expenses.js
@@ -4,70 +4,70 @@ const router = Router();
 const expenseData = require('../data/expenses');
 
 //Add Expense of trip
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const expense = req.body;
     const newExpense = await expenseData.addExpense(expense);
     res.status(200).json(newExpense);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Get All expenses
-router.get('/trip/:tripId', async (req, res) => {
+router.get('/trip/:tripId', async (req, res, next) => {
   try {
     const { tripId } = req.params;
 
     const allExpenses = await expenseData.getAllExpensesByTrip(tripId);
     res.status(200).json(allExpenses);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Get single expense by expenseId
-router.get('/:expenseId', async (req, res) => {
+router.get('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
     const expense = await expenseData.getExpense(expenseId);
     res.status(200).json(expense);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Update expense
-router.put('/:expenseId', async (req, res) => {
+router.put('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
     let expense = req.body;
     expense = await expenseData.updateExpense(expenseId, expense);
     res.status(200).json(expense);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Delete all expenses
-router.delete('/trip/:tripId', async (req, res) => {
+router.delete('/trip/:tripId', async (req, res, next) => {
   try {
     const { tripId } = req.params;
     const allExpenses = await expenseData.deleteAllExpensesByTrip(tripId);
     res.status(200).json(allExpenses);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
 //Delete single expense by expenseId
-router.delete('/:expenseId', async (req, res) => {
+router.delete('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
     const expense = await expenseData.deleteExpense(expenseId);
     res.status(200).json(expense);
-  } catch (e) {
-    res.status(400).json({ error: e });
+  } catch (error) {
+    next(error);
   }
 });
 
