388fbcc52c53fe8fe683e7fb8d00da5781b029f6
parthikdavra <62086803+parthikdavra@users.noreply.github.com>
2021-04-30 12:36:22 -0400

    Add corporate Routes

--- data/corporate.js
+++ data/corporate.js
@@ -2,12 +2,9 @@ const { ObjectId, ObjectID } = require("mongodb");
 const {
   corporate: getCorporatesCollection,
 } = require("../config/mongoCollections");
+const { QueryError } = require("../utils/errors");
+const { idQuery, parseMongoData } = require("../utils/mongodb");
 
-const { QueryError, ValidationError } = require("../utils/errors");
-const {
-  idQuery,
-  parseMongoData
-} = require("../utils/mongodb");
 const {
   assertObjectIdString,
   assertIsValuedString,
@@ -15,137 +12,157 @@ const {
   assertRequiredNumber,
 } = require("../utils/assertion");
 
+const isDataExist = (id, data, desc = "data") => {
+  if (!data)
+    throw new QueryError(`Cannot find corporate data with ID: ${id}`, 404);
+};
+
 const getByObjectId = async (objectId) => {
-    const collection = await getCorporatesCollection();
-    const corporate = await collection.findOne(idQuery(objectId));
-    return parseMongoData(corporate);
+  const collection = await getCorporatesCollection();
+  const corporate = await collection.findOne(idQuery(objectId));
+  isDataExist(objectId, corporate, "corporate");
+
+  return parseMongoData(corporate);
 };
 
 const getAllCorporates = async () => {
-    const collection = await getCorporatesCollection();
+  const collection = await getCorporatesCollection();
 
-    const corporateList = await collection.find({}).toArray();
+  const corporateList = await collection.find({}).toArray();
 
-    return parseMongoData(corporateList);
-},
-  
+  return parseMongoData(corporateList);
+};
+
 const getCorporate = async (id) => {
-    assertObjectIdString(id);
-    return await getByObjectId(new ObjectId(id));
+  assertObjectIdString(id);
+  return await getByObjectId(new ObjectId(id));
 };
 
 const createCorporate = async (data) => {
-    assertRequiredObject(data);
-  
-    const { name, emailDomain, contactNo, address, createdBy, updatedBy, createdAt = new Date().getTime() } = data;
-     
-    assertIsValuedString(name, "Corporate name");
-    assertIsValuedString(emailDomain, "Email");
-    assertRequiredNumber(contactNo, "Contact Number");
-    assertIsValuedString(address, "Address");
-    assertObjectIdString(createdBy, "Created By");
-    assertObjectIdString(updatedBy, "Update By");
-    assertRequiredNumber(createdAt, "Corporate created time");
-  
-    const corporateData = {
-      _id: new ObjectId(),
-      name : name,
-      emailDomain: emailDomain,
-      contactNo: contactNo,
-      address: address,
-      createdBy: createdBy,
-      updatedBy: updatedBy,
-      createdAt: createdAt,
-      updatedAt: createdAt,
-    };
-  
-    const collection = await getCorporatesCollection();
-    const { result, insertedCount, insertedId } = await collection.insertOne(
-      corporateData
+  assertRequiredObject(data);
+
+  const {
+    name,
+    emailDomain,
+    contactNo,
+    address,
+    createdBy,
+    updatedBy,
+    createdAt = new Date().getTime(),
+  } = data;
+  assertIsValuedString(name, "Corporate name");
+  assertIsValuedString(emailDomain, "Email");
+  assertIsValuedString(contactNo, "Contact Number");
+  assertIsValuedString(address, "Address");
+  assertRequiredObject(createdBy, "Created By");
+  assertRequiredObject(updatedBy, "Update By");
+  assertRequiredNumber(createdAt, "Corporate created time");
+
+  const corporateData = {
+    _id: new ObjectId(),
+    name: name,
+    emailDomain: emailDomain,
+    contactNo: contactNo,
+    address: address,
+    createdBy: createdBy,
+    updatedBy: updatedBy,
+    createdAt: createdAt,
+    updatedAt: createdAt,
+  };
+  const collection = await getCorporatesCollection();
+  const { result, insertedCount, insertedId } = await collection.insertOne(
+    corporateData
+  );
+
+  if (!result.ok || insertedCount !== 1) {
+    throw new QueryError(
+      `Could not create corporate for corporate Email(${email})`
     );
-  
-    if (!result.ok || insertedCount !== 1) {
-      throw new QueryError(`Could not create corporate for corporate Email(${email})`);
-    }
-  
-    return await getByObjectId(insertedId);
+  }
+
+  return await getByObjectId(insertedId);
 };
 
 const updateCorporate = async (id, updates) => {
-    assertObjectIdString(id);
-    assertRequiredObject(updates, "Corporate updates data");
+  assertObjectIdString(id);
+  assertRequiredObject(updates, "Corporate updates data");
 
-    const { name, emailDomain, contactNo, address, createdBy, updatedBy, createdAt = new Date().getTime() } = data;
-    
-    assertIsValuedString(name, "Corporate name");
-    assertIsValuedString(emailDomain, "Email");
-    assertRequiredNumber(contactNo, "Contact Number");
-    assertIsValuedString(address, "Address");
-    assertObjectIdString(createdBy, "Created By");
-    assertObjectIdString(updatedBy, "Update By");
-    assertRequiredNumber(createdAt, "Corporate created time");
-  
-    const corporate = await getCorporate(id);
+  const {
+    name,
+    emailDomain,
+    contactNo,
+    address,
+    updatedBy,
+    updatedAt = new Date().getTime(),
+  } = updates;
 
-    if (!corporate) {
-      throw new QueryError(`Corporate with ID\`${id}\` not found.`);
-    }
+  assertIsValuedString(name, "Corporate name");
+  assertIsValuedString(emailDomain, "Email");
+  assertIsValuedString(contactNo, "Contact Number");
+  assertIsValuedString(address, "Address");
+  assertRequiredObject(updatedBy, "Update By");
+  assertRequiredNumber(updatedAt, "Corporate Updated time");
 
-    // TODO: validate session user ID and operation
-  
-    const options = { returnOriginal: false };
-    const collection = await getCorporatesCollection();
-    const currentTimestamp = new Date().getTime();
-    
-    const newUpdate = {
-      name : name,
-      emailDomain: emailDomain,
-      contactNo: contactNo,
-      address: address,
-      createdBy: createdBy,
-      updatedBy: updatedBy,
-      createdAt: createdAt,
-      updatedAt: currentTimestamp,
-    };
+  const corporate = await getCorporate(id);
 
-    const ops = {
-      $set: newUpdate      
-    };
-  
-    const { value: updatedCorporate, ok } = await collection.findOneAndUpdate(
-      idQuery(id),
-      ops,
-      options
-    );
-  
-    if (!ok) {
-      throw new QueryError(`Could not update rank with ID \`${id}\``);
-    }
-  
-    return parseMongoData(updatedCorporate);
+  if (!corporate) {
+    throw new QueryError(`Corporate with ID\`${id}\` not found.`);
+  }
+
+  const options = { returnOriginal: false };
+  const collection = await getCorporatesCollection();
+
+  const newUpdate = {
+    name: name,
+    emailDomain: emailDomain,
+    contactNo: contactNo,
+    address: address,
+    updatedBy: updatedBy,
+    updatedAt: updatedAt,
+  };
+
+  const ops = {
+    $set: newUpdate,
+  };
+
+  const { value: updatedCorporate, ok } = await collection.findOneAndUpdate(
+    idQuery(id),
+    ops,
+    options
+  );
+
+  if (!ok) {
+    throw new QueryError(`Could not update rank with ID \`${id}\``);
+  }
+
+  return parseMongoData(updatedCorporate);
 };
 
 const deleteCorporate = async (id) => {
-    const collection = await getCorporatesCollection();  
+  assertObjectIdString(id);
+  const collection = await getCorporatesCollection();
 
-    const deletionInfo = await collection.deleteOne({ _id: idQuery(id) });
+  const corporate = await getCorporate(id);
+  if (!corporate) {
+    throw new QueryError(`Corporate with ID\`${id}\` not found.`);
+  }
 
-    if (deletionInfo.deletedCount === 0) {
-        throw new QueryError(`Could not delete corporate with id of ${id}`);
-    }
+  const deletionInfo = await collection.deleteOne({ _id: new ObjectId(id) });
+  if (deletionInfo.deletedCount === 0) {
+    throw new QueryError(`Could not delete corporate with id of ${id}`);
+  }
 
-    if (deletionInfo.deletedCount > 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+  if (deletionInfo.deletedCount > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = {
-    createCorporate,
-    getCorporate,
-    getAllCorporates,
-    updateCorporate,
-    deleteCorporate
+  createCorporate,
+  getCorporate,
+  getAllCorporates,
+  updateCorporate,
+  deleteCorporate,
 };
