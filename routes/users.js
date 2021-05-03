8a4d3adca015bc5b09c13cf8b10d8c834f03b68c
dbrittovivian <dbritto.vivian@gmail.com>
2021-05-03 16:33:41 -0400

    Login & logout routes added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-
+const bcrypt = require('bcrypt');
 const usersData = require('../data/users');
 
 //add user
@@ -24,17 +24,6 @@ try {
 }
 });
 
-//get user by Id
-router.get('/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await usersData.getUser(userId);
-    res.status(200).json(user);
-  } catch (e) {
-    res.status(400).json({ error: e });
-  }
-});
-
 //Update user
 router.put('/:userId', async (req, res) => {
     try {
@@ -56,6 +45,79 @@ router.delete('/:userId', async (req, res) => {
     } catch (e) {
       res.status(400).json({ error: e });
     }
-  });
+});
+
+router.post('/login', async (req, res) => {
+    const reqBody = req.body;
+    let errors = [];
+    let hasErrors = false;
+
+    const { email, password } = reqBody;
+
+    if (!email || typeof email !== 'string' || email.trim().length === 0) {
+        errors.push('Email is empty or not of required format.');
+    }
+
+    if (!password || typeof password !== 'string' || password.trim().length === 0) {
+        errors.push('Password is empty or not of required format.');
+    }
+
+    if (errors.length > 0) {
+        // res.status(401)
+        //    .render('login', { errors : errors, hasErrors : true, userInfo : userInfo, title : title });
+        res.status(401).json({ errors : errors });
+        return;
+    }
+
+    const user = await usersData.getUserByEmail(email);
+
+    if (!user) {
+        errors.push(`No user with ${email} found.`); 
+        res.status(401).json({ errors : errors });
+
+        // TODO: HTTP 401 status code
+        // res.status(401)
+        //    .render('login', { errors : errors, hasErrors : true, userInfo : userInfo, title : title });
+        return;
+    }
+
+    user.password = await bcrypt.hash(user.password, 8);
+
+    let match = await bcrypt.compare(password, user.password);
+
+    if (user.email === email && match) {
+        req.session.user = user;
+        res.status(200).json(user);
+        // res.redirect('/private');
+    }
+    else {
+        errors.push('Invalid username or password.');
+        res.status(401).json({ errors : errors });
+
+        // res.status(401)
+        //    .render('login', { errors : errors, hasErrors : true, userInfo : userInfo, title : title });
+        return;
+    }    
+});
+
+router.get('/logout', async (req, res) => {
+  try {  
+      req.session.destroy();
+      res.status(200).json({ message : 'User logged out' });
+  } catch (e) {
+      res.status(400).json({ error: e });
+  }
+});
+
+//get user by Id
+router.get('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await usersData.getUser(userId);
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).json({ error: e });
+  }
+});
 
 module.exports = router;
