815efd3a0f3da3d2a47d65a2d8d4fd616dd6b1d0
Smit Gor <smitgor1620@gmail.com>
2021-05-04 14:32:00 -0400

    fixed routes

--- routes/expenses.js
+++ routes/expenses.js
@@ -3,11 +3,33 @@ const router = Router();
 
 const expenseData = require('../data/expenses');
 
+const {
+  assertObjectIdString,
+  assertRequiredObject,
+  assertIsValuedString,
+} = require('../utils/assertion');
+
+const { HttpError } = require('../utils/errors');
+
+const assertExpenseData = (expenseData) => {
+  assertObjectIdString(expenseData.userId, 'User ID');
+  assertObjectIdString(expenseData.tripId, 'Trip ID');
+  assertIsValuedString(expenseData.name, 'Expense name');
+  assertDateString(expenseData.date, 'Payment date');
+  assertRequiredNumber(expenseData.amount, 'Expense amount');
+  assertIsValuedString(expenseData.currency, 'Expense curreny');
+  assertIsValuedString(expenseData.method, 'Payment method');
+};
+
 //Add Expense of trip
 router.post('/', async (req, res, next) => {
   try {
     const expense = req.body;
+    assertExpenseData(expense);
     const newExpense = await expenseData.addExpense(expense);
+    if (!newExpense) {
+      throw new HttpError(`Could not add new expense`);
+    }
     res.status(200).json(newExpense);
   } catch (error) {
     next(error);
@@ -18,8 +40,11 @@ router.post('/', async (req, res, next) => {
 router.get('/trip/:tripId', async (req, res, next) => {
   try {
     const { tripId } = req.params;
-
+    assertObjectIdString(tripId);
     const allExpenses = await expenseData.getAllExpensesByTrip(tripId);
+    if (!allExpenses) {
+      throw new HttpError(`Expense not found with provided trip id: ${tripId}`, 404);
+    }
     res.status(200).json(allExpenses);
   } catch (error) {
     next(error);
@@ -30,7 +55,11 @@ router.get('/trip/:tripId', async (req, res, next) => {
 router.get('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
+    assertObjectIdString(expenseId);
     const expense = await expenseData.getExpense(expenseId);
+    if (!expense) {
+      throw new HttpError(`Expense not found for expense id:${expenseId}`, 404);
+    }
     res.status(200).json(expense);
   } catch (error) {
     next(error);
@@ -42,7 +71,12 @@ router.put('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
     let expense = req.body;
+    assertObjectIdString(expenseId);
+    assertExpenseData(expense);
     expense = await expenseData.updateExpense(expenseId, expense);
+    if (!expense) {
+      throw new HttpError(`Could not update expense for id:${expenseId}`, 404);
+    }
     res.status(200).json(expense);
   } catch (error) {
     next(error);
@@ -53,7 +87,11 @@ router.put('/:expenseId', async (req, res, next) => {
 router.delete('/trip/:tripId', async (req, res, next) => {
   try {
     const { tripId } = req.params;
+    assertObjectIdString(tripId);
     const allExpenses = await expenseData.deleteAllExpensesByTrip(tripId);
+    if (!allExpenses) {
+      throw new HttpError(`Could not delete all expenses for trip id:${tripId}`, 404);
+    }
     res.status(200).json(allExpenses);
   } catch (error) {
     next(error);
@@ -64,7 +102,11 @@ router.delete('/trip/:tripId', async (req, res, next) => {
 router.delete('/:expenseId', async (req, res, next) => {
   try {
     const { expenseId } = req.params;
+    assertObjectIdString(expenseId);
     const expense = await expenseData.deleteExpense(expenseId);
+    if (!expense) {
+      throw new HttpError(`Could not delete expense for id:${expenseId}`, 404);
+    }
     res.status(200).json(expense);
   } catch (error) {
     next(error);
