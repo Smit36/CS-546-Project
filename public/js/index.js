233ded7df1bbab15d05c4d485699e6cf860999b4
parthikdavra <62086803+parthikdavra@users.noreply.github.com>
2021-05-04 16:38:30 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,3 @@ $(document).ready(function () {
   console.log($);
 });
 
-const link =  $('#id')
-
-$('a').click(function(){
-  $.ajax({
-  type: "GET",
-  url: "mysimplepage.php",
-  async: true,
-  data: { logDownload: true, file: $(this).attr("name") }
-  });
-  return false;
-  });
