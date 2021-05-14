2255d428a0c427593a89f66e8fc9c1f6920b2c78
parthikdavra <62086803+parthikdavra@users.noreply.github.com>
2021-05-14 17:29:42 -0400

    Solve Errors

--- public/js/corporate.js
+++ public/js/corporate.js
@@ -27,7 +27,7 @@ create.on("click", function (event) {
     <div id='input'>
     <label for="name" >Corporate Name
       <input type="text" id="name" placeholder="Enter Name" pattern="^[a-zA-Z ']{2,50}$" required>
-      <span id="name-error" hidden>Please Enter Valid Name</apan>
+      <span id="names-error" hidden>Please Enter Valid Name</apan>
     </label>
     </div>
     <div>
@@ -60,7 +60,7 @@ create.on("click", function (event) {
     var corporate_contact = $("#contact").val();
     var corporate_address = $("#address").val();
 
-    const validate = corporateEmptyValidation();
+    const validate = corporateEmptyValidationCreate();
     
     var data = {
       name: corporate_name,
@@ -135,7 +135,7 @@ edit.on("click", function (event) {
     <div>
     <label for="name" >Corporate Name
       <input type="text" id="name" placeholder="Enter Name" pattern="^[a-zA-Z ']{2,50}$" required>
-      <span id="name-error" hidden>Please Enter Valid Name</apan>
+      <span id="names-error" hidden>Please Enter Valid Name</apan>
     </label>
     </div>
     <div>
@@ -163,7 +163,7 @@ edit.on("click", function (event) {
     var corporate_contact = $("#contact").val();
     var corporate_address = $("#address").val();
 
-    const validate = corporateEmptyValidation();
+    const validate = corporateEmptyValidationEdit();
 
     var data = {
       name: corporate_name,
@@ -178,6 +178,7 @@ edit.on("click", function (event) {
         type: "PUT",
         data: data,
         success(data) {
+          console.log(data)
           window.location.reload();
         },
       });
@@ -185,16 +186,56 @@ edit.on("click", function (event) {
   });
 });
 
-// Validation for Input Field
-function corporateEmptyValidation() {
+// Validation for Input Field for Create
+function corporateEmptyValidationCreate() {
   event.preventDefault();
   let error = 0;
   if (!$("#name").val()) {
-    $("#name-error").show();
+    $("#names-error").show();
     error += 1;
   } else {
-    $("#name-error").hide();
+    $("#names-error").hide();
   }
+  if (!$("#email").val() || !($("#email").val().match(/^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/))) {
+    $("#email-error").show();
+    error += 1;
+  } else {
+    $("#email-error").hide();
+  }
+
+  if (!$("#contact").val() || !($("#contact").val().match(/^\(?([0-9]{3})\)?[- ]+?([0-9]{3})[- ]+?([0-9]{4})$/))) {
+    $("#contact-error").show();
+    error += 1;
+  } else {
+    $("#contact-error").hide();
+  }
+  if (!$("#address").val()) {
+    $("#address-error").show();
+    error += 1;
+  } else {
+    $("#address-error").hide();
+  }
+ 
+  contactError = false;
+  emailError = false;
+  if (error === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Validation for Input Field for Edit
+function corporateEmptyValidationEdit() {
+  event.preventDefault();
+  let error = 0;
+  if (!$("#name").val()) {
+    $("#names-error").show();
+    error += 1;
+  } else {
+    $("#names-error").hide();
+  }
+
   if (!$("#contact").val() || !($("#contact").val().match(/^\(?([0-9]{3})\)?[- ]+?([0-9]{3})[- ]+?([0-9]{4})$/))) {
     $("#contact-error").show();
     error += 1;
@@ -219,6 +260,11 @@ function corporateEmptyValidation() {
 
 // Error Checking
 function showError(){
+  if (emailError) {
+    $("#email-error").show();
+  } else {
+    $("#email-error").hide();
+  }
   if (contactError) {
       $("#contact-error").show();
     } else {
