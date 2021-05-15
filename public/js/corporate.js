e49d088f7003e0f661dc29c8dfae98e84e6eca9c
parthikdavra <62086803+parthikdavra@users.noreply.github.com>
2021-05-15 10:51:44 -0400

    update html validation

--- public/js/corporate.js
+++ public/js/corporate.js
@@ -1,13 +1,13 @@
-var list = $('.link').val();
-var del = $('#delete');
-var card = $('.single-card');
-var info = $('#title-info');
-var edit = $('#edit');
-var back = $('#previous');
-var create = $('.corporate-create');
-var createAdd = $('.add-create');
-var corporateList = $('.corporates-list');
-var error = $('.container-error');
+var list = $(".link").val();
+var del = $("#delete");
+var card = $(".single-card");
+var info = $("#title-info");
+var edit = $("#edit");
+var back = $("#previous");
+var create = $(".corporate-create");
+var createAdd = $(".add-create");
+var corporateList = $(".corporates-list");
+var error = $(".container-error");
 card.show();
 del.show();
 edit.show();
@@ -16,7 +16,7 @@ var emailError = false;
 var contactError = false;
 
 // Create Corporate Button Click
-create.on('click', function (event) {
+create.on("click", function (event) {
   create.hide();
   event.preventDefault();
   corporateList.hide();
@@ -27,7 +27,7 @@ create.on('click', function (event) {
     <div id='input'>
     <label for="name" >Corporate Name
       <input type="text" id="name" placeholder="Enter Name" pattern="^[a-zA-Z ']{2,50}$" required>
-      <span id="names-error" hidden>Please Enter Valid Name</apan>
+      <span id="names-error" hidden>Please Enter Valid Name</span>
     </label>
     </div>
     <div>
@@ -52,17 +52,16 @@ create.on('click', function (event) {
     <a href="/corporates" class='nav' id="back">Back</a>
   </form>`);
 
-  var submit_create = $('.submit-create');
+  var submit_create = $(".submit-create");
 
-  submit_create.on('click', function (event) {
-    var corporate_name = $('#name').val();
-    var corporate_email = $('#email').val();
-    var corporate_contact = $('#contact').val();
-    var corporate_address = $('#address').val();
-
+  submit_create.on("click", function (event) {
+    var corporate_name = $("#name").val();
+    var corporate_email = $("#email").val();
+    var corporate_contact = $("#contact").val();
+    var corporate_address = $("#address").val();
 
     const validate = corporateEmptyValidationCreate();
-  
+
     var data = {
       name: corporate_name,
       emailDomain: corporate_email,
@@ -72,16 +71,16 @@ create.on('click', function (event) {
 
     if (validate) {
       $.ajax({
-        url: '/corporates',
-        type: 'POST',
+        url: "/corporates",
+        type: "POST",
         data: data,
         success(data) {
           window.location.replace(`/corporates/${data.corporate._id}`);
         },
         error(error) {
-          if (error.responseJSON.message.includes('Email')) {
+          if (error.responseJSON.message.includes("Email")) {
             emailError = true;
-          } else if (error.responseJSON.message.includes('Contact')) {
+          } else if (error.responseJSON.message.includes("Contact")) {
             contactError = true;
           }
           showError();
@@ -93,15 +92,19 @@ create.on('click', function (event) {
 corporateList.show();
 
 // Delete Corporate Button Click
-del.on('click', function (event) {
+del.on("click", function (event) {
   event.preventDefault();
-  let url = $(this).attr('href');
+  let url = $(this).attr("href");
   $.ajax({
     url: url,
-    type: 'DELETE',
+    type: "DELETE",
     success(data) {
       card.hide();
-      info.append(jQuery(document.createElement('h1')).text('Corporate Deleted Succesfully.'));
+      info.append(
+        jQuery(document.createElement("h1")).text(
+          "Corporate Deleted Succesfully."
+        )
+      );
       del.hide();
       edit.hide();
       back.hide();
@@ -110,17 +113,17 @@ del.on('click', function (event) {
   });
 });
 
-var container = $('.container-edit');
-var container_info = $('.container-info');
-var container_update = $('.container-update');
+var container = $(".container-edit");
+var container_info = $(".container-info");
+var container_update = $(".container-update");
 container_update.hide();
 
 // Edit Corporate Button Click
-edit.on('click', function (event) {
+edit.on("click", function (event) {
   event.preventDefault();
   del.hide();
   edit.hide();
-  let co_email = $(this).attr('name');
+  let co_email = $(this).attr("name");
 
   $(container).append(
     `<form>
@@ -129,10 +132,11 @@ edit.on('click', function (event) {
     <hr>
     <div>
       <h2>Corporate Domain: ${co_email}</h2>
+    </div>
     <div>
     <label for="name" >Corporate Name
       <input type="text" id="name" placeholder="Enter Name" pattern="^[a-zA-Z ']{2,50}$" required>
-      <span id="names-error" hidden>Please Enter Valid Name</apan>
+      <span id="names-error" hidden>Please Enter Valid Name</span>
     </label>
     </div>
     <div>
@@ -148,16 +152,16 @@ edit.on('click', function (event) {
     </label>
     </div>
     <input type="submit" value="Submit" class="submit-edit"  />
-  </form>`,
+  </form>`
   );
 
-  var submit_edit = $('.submit-edit');
-  let edit_url = $(this).attr('href');
+  var submit_edit = $(".submit-edit");
+  let edit_url = $(this).attr("href");
 
-  submit_edit.on('click', function (event) {
-    var corporate_name = $('#name').val();
-    var corporate_contact = $('#contact').val();
-    var corporate_address = $('#address').val();
+  submit_edit.on("click", function (event) {
+    var corporate_name = $("#name").val();
+    var corporate_contact = $("#contact").val();
+    var corporate_address = $("#address").val();
 
     const validate = corporateEmptyValidationEdit();
 
@@ -171,10 +175,10 @@ edit.on('click', function (event) {
       event.preventDefault();
       $.ajax({
         url: edit_url,
-        type: 'PUT',
+        type: "PUT",
         data: data,
         success(data) {
-          console.log(data)
+          console.log(data);
           window.location.reload();
         },
       });
@@ -193,14 +197,26 @@ function corporateEmptyValidationCreate() {
   } else {
     $("#names-error").hide();
   }
-  if (!$("#email").val() || !($("#email").val().match(/^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/))) {
+  if (
+    !$("#email").val() ||
+    !$("#email")
+      .val()
+      .match(
+        /^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/
+      )
+  ) {
     $("#email-error").show();
     error += 1;
   } else {
     $("#email-error").hide();
   }
 
-  if (!$("#contact").val() || !($("#contact").val().match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/))) {
+  if (
+    !$("#contact").val() ||
+    !$("#contact")
+      .val()
+      .match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+  ) {
     $("#contact-error").show();
     error += 1;
   } else {
@@ -212,7 +228,7 @@ function corporateEmptyValidationCreate() {
   } else {
     $("#address-error").hide();
   }
- 
+
   contactError = false;
   emailError = false;
   if (error === 0) {
@@ -233,17 +249,22 @@ function corporateEmptyValidationEdit() {
     $("#names-error").hide();
   }
 
-  if (!$("#contact").val() || !($("#contact").val().match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/))) {
+  if (
+    !$("#contact").val() ||
+    !$("#contact")
+      .val()
+      .match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+  ) {
     $("#contact-error").show();
     error += 1;
   } else {
-    $('#contact-error').hide();
+    $("#contact-error").hide();
   }
-  if (!$('#address').val()) {
-    $('#address-error').show();
+  if (!$("#address").val()) {
+    $("#address-error").show();
     error += 1;
   } else {
-    $('#address-error').hide();
+    $("#address-error").hide();
   }
 
   contactError = false;
@@ -257,15 +278,15 @@ function corporateEmptyValidationEdit() {
 
 // Error Checking
 
-function showError(){
+function showError() {
   if (emailError) {
     $("#email-error").show();
   } else {
     $("#email-error").hide();
   }
   if (contactError) {
-    $('#contact-error').show();
+    $("#contact-error").show();
   } else {
-    $('#contact-error').hide();
+    $("#contact-error").hide();
   }
 }
